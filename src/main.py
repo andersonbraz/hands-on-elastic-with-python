49c49aefaaf64eab4e12de948355fba4b60bf05d
Anderson Braz de Sousa <anderson.sousa@bancointer.com.br>
2022-01-14 12:56:33 -0300

    Up File(s).

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
-def main():
-    create_index_company()
+from datetime import datetime
+from elasticsearch import Elasticsearch
 
+es = Elasticsearch()
 
-def create_index_company():
-    print("Create Index: Company.")
+doc = {
+    "author": "kimchy",
+    "text": "Elasticsearch: cool. bonsai cool.",
+    "timestamp": datetime.now(),
+}
+res = es.index(index="inter-sample", id=1, document=doc)
+print(res["result"])
 
+res = es.get(index="inter-sample", id=1)
+print(res["_source"])
 
-if __name__ == "__main__":
-    main()
+es.indices.refresh(index="inter-sample")
+
+res = es.search(index="inter-sample", query={"match_all": {}})
+print("Got %d Hits:" % res["hits"]["total"]["value"])
+for hit in res["hits"]["hits"]:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
